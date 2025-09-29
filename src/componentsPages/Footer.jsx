f642177607118e1c1054b37b21e0f6581e284d02
NakodMansi <mansivnakod1007@gmail.com>
2025-09-29 16:49:37 +0530

    header footer changes

--- src/componentsPages/Footer.jsx
+++ src/componentsPages/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <div className="font-palanquin text-[#FFF] p-4 flex flex-col items-center w-full gap-3 bg-[#0B0B0B] md:py-6 md:px-8">
             <ul className="flex justify-center items-center gap-4 self-stretch md:hidden">
-                <a className="text-[clamp(0.6rem,1vw,2vw)] leading-[clamp(1rem,2vw,3rem)]" href="/about">
+                <Link className="text-[clamp(0.6rem,1vw,2vw)] leading-[clamp(1rem,2vw,3rem)]" href="/about">
                     <li>About</li>
-                </a>
+                </Link>
 
-                <a className="text-[clamp(0.6rem,1vw,2vw)] leading-[clamp(1rem,2vw,3rem)]" href="/contactUs">
+                <Link className="text-[clamp(0.6rem,1vw,2vw)] leading-[clamp(1rem,2vw,3rem)]" href="/contactUs">
                     <li className="">Contact</li>
-                </a>
+                </Link>
             </ul>
 
             <hr className="w-full w-[0.1px] bg-[#FFF] md:hidden"/> 
@@ -19,13 +20,13 @@ export default function Footer() {
                 <p className="text-[clamp(0.6rem,1vw,2vw)] leading-[clamp(1rem,2vw,3rem)]">© 2025 Politicker. All rights reserved.</p>
 
                 <ul className="hidden md:flex md:justify-center md:items-center md:gap-4 md:self-stretch">
-                    <a href="/about">
+                    <Link href="/about">
                         <li>About</li>
-                    </a>
+                    </Link>
 
-                    <a href="/contactUs">
+                    <Link href="/contactUs">
                         <li>Contact</li>
-                    </a>
+                    </Link>
                 </ul>
 
                 <ul className="flex gap-2 justify-end md:gap-4">
--- src/componentsPages/Header.jsx
+++ src/componentsPages/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
     return (
         <div className="flex py-5 px-4 items-center justify-between bg-[#191919] text-[#FFF] h-[44px] md:px-8 lg:px-10 w-full">
             <ul className="flex justify-center items-center gap-3 md:gap-6">
-                <a href="/about">
+                <Link href="/about">
                     <li className="text-[clamp(0.6rem,1vw,2rem)] leading-[clamp(1rem,2vw,3rem)]">about</li>
-                </a>
+                </Link>
 
-                <a href="/contactUs">
+                <Link href="/contactUs">
                     <li className="text-[clamp(0.6rem,1vw,2rem)] leading-[clamp(1rem,2vw,3rem)]">contact</li>
-                </a>
+                </Link>
             </ul>
 
             <p className="text-[clamp(1.2rem,2vw,3rem)] leading-[clamp(1.5rem,2vw,3rem)]">Politicker</p>
