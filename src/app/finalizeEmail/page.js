9b4599dcd5de3b6cd922959cec74538ab54ac974
NakodMansi <mansivnakod1007@gmail.com>
2025-10-02 17:31:36 +0530

    wrapped useSearchParams in suspense

--- src/app/finalizeEmail/page.js
+++ src/app/finalizeEmail/page.js
@@ -1,31 +1,42 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "@/componentsPages/Header";
 import Footer from "@/componentsPages/Footer";
 import ContactUs from "@/componentsPages/ContactUs";
 import Content from "../mail/content.json";
-import { useParams, useSearchParams } from "next/navigation";
 
-const data = ["Create a proton mail", "Recipients (bcc)", "Subject","Email"]
+// Data for form
+const data = ["Create a proton mail", "Recipients (bcc)", "Subject","Email"];
+
+// Client-side component to handle searchParams
+function ContactUsWrapper({ data, inputValues }) {
+  const { useSearchParams } = require("next/navigation"); // dynamically require if needed
+  const searchParams = useSearchParams();
+  const templateNo = searchParams.get("templateNo");
+
+  return (
+    <ContactUs
+      heading="3. finalize your emails"
+      button1="create a proton mail"
+      button2="next email"
+      contactUsData={data}
+      inputValues={inputValues}
+      templateNo={templateNo}
+      showProtonMail={false}
+      showCopyImage={true}
+      show={true}
+    />
+  );
+}
+
 export default function FinalizeEmail() {
-    const searchParams = useSearchParams();
-    const templateNo = searchParams.get("templateNo");
-
-    return (
-        <>  
-        <Header />
-        <ContactUs 
-            heading= "3. finalize your emails"
-            button1 = "create a proton mail"
-            button2 = "next email"
-            contactUsData = {data}
-            inputValues = {Content}
-            templateNo = {templateNo}
-            showProtonMail = {false}
-            showCopyImage = {true}
-            show = {true}
-        />
-        <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<div>Loading email form...</div>}>
+        <ContactUsWrapper data={data} inputValues={Content} />
+      </Suspense>
+      <Footer />
+    </>
+  );
 }
